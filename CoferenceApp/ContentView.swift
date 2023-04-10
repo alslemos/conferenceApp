42ceede4b24877b81c25ecefb74d68b9fd3f8055
filipeilunga <filipeilunga@gmail.com>
2023-04-10 16:13:40 -0300

    Criando a tabBar

--- CoferenceApp/ContentView.swift
+++ CoferenceApp/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentPage: Int = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView(selection: $currentPage) {
+            
+            Home().tabItem {
+                Text("Home")
+                Image(systemName: "house.fill")
+            }.tag(1)
+            
+            Events().tabItem {
+                Text("Events")
+                Image(systemName: "calendar")
+            }.tag(3)
+            
+            Maps().tabItem {
+                Text("Maps")
+                Image(systemName: "map")
+            }.tag(4)
+            
+            AroundMe().tabItem {
+                Text("Events")
+                Image(systemName: "person.line.dotted.person.fill")
+            }.tag(5)
+            
+            Profile().tabItem {
+                Text("Profile")
+                Image(systemName: "person")
+            }.tag(6)
         }
-        .padding()
     }
 }
 
--- CoferenceApp/Pages/Events.swift
+++ CoferenceApp/Pages/Events.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct Events: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Events Page")
     }
 }
 
