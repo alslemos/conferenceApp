863850f4a347b0dd06128be9db1327854efaa654
filipeilunga <filipeilunga@gmail.com>
2023-04-10 15:46:51 -0300

    Initial Commit

---
+++ CoferenceApp/CoferenceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoferenceAppApp.swift
+//  CoferenceApp
+//
+//  Created by Filipe Ilunga on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CoferenceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
