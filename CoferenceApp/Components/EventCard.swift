b004cb713c0fb9dacbb2a48c12927b230a0e1a50
filipeilunga <filipeilunga@gmail.com>
2023-04-11 10:09:15 -0300

    Criado componente de eventos

---
+++ CoferenceApp/Components/EventCard.swift
@@ -0,0 +1,56 @@
+//
+//  EventCard.swift
+//  CoferenceApp
+//
+//  Created by Filipe Ilunga on 10/04/23.
+//
+
+import SwiftUI
+
+struct EventCard: View {
+    var body: some View {
+        HStack(spacing: 15) {
+            VStack(alignment: .leading, spacing: 6) {
+                HStack {
+                    Circle()
+                        .frame(width: 36, height: 36)
+                    Text("Lynn Streja")
+                }
+                Text("Everthing about the new programming language Swift")
+                    .font(.subheadline)
+                    .bold()
+                Text("@Steve Jobs Theater")
+                    .font(.caption)
+
+            }.padding(.vertical)
+            
+            VStack(spacing: 25) {
+                HStack {
+                    Image(systemName: "map")
+                        .foregroundColor(.white)
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.white)
+                }
+                Text("1:30pm")
+                    .font(.caption)
+                    .padding(.horizontal, 6)
+                    .padding(.vertical, 3)
+                    .background(Color.white)
+                    .foregroundColor(.teal)
+                    .cornerRadius(27)
+            }
+        }.frame(width: 342)
+        .background(Color.teal)
+        .cornerRadius(16)
+        .foregroundColor(.white)
+        .shadow(color: .black, radius: 4, x: 0 , y: 10)
+        .padding(.horizontal)
+    }
+}
+
+struct EventCard_Previews: PreviewProvider {
+    static var previews: some View {
+        EventCard()
+            .previewLayout(.fixed(width: 342, height: 124))
+    }
+}
