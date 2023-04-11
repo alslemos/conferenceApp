d84cd13e498a1433fa93f7a71e98bdd62c5b042a
julia gusmao <jgusmao23@epoa.idserve.net>
2023-04-11 14:56:43 -0300

    criei a tela de perfil

--- CoferenceApp/Pages/Profile.swift
+++ CoferenceApp/Pages/Profile.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct Profile: View {
+    let configs: [String] = ["Settings", "App Information", "Privacy & Security", "Log Out"]
     var body: some View {
-        Text("Profile Profile - commit")
+        VStack{
+            
+            
+            HStack(alignment: .top){
+                Image(systemName: "apple.logo").font(.largeTitle)
+                Text("WWDC23").font(.largeTitle).bold()
+                
+            }
+        
+            Image("perfil")
+                .clipShape(Circle())
+            Spacer()
+            List {
+                
+                ForEach(configs, id: \.self) { item in
+                    
+                    HStack {
+                        Text(item).foregroundColor(.black)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                    }
+                    
+                }
+            }
+        }
+        .padding(.top,40)
+        .background(Color(uiColor: UIColor.secondarySystemBackground).edgesIgnoringSafeArea(.all))
+        
+        
     }
 }
+
 
 struct Profile_Previews: PreviewProvider {
     static var previews: some View {
