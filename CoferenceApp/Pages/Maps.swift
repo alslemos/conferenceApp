04dd054cc1613b573b57a68ba5c1e817d1c61d4c
filipeilunga <filipeilunga@gmail.com>
2023-04-11 12:20:29 -0300

    Feature/Mapa interativo base

--- CoferenceApp/Pages/Maps.swift
+++ CoferenceApp/Pages/Maps.swift
@@ -7,17 +7,39 @@
 
 import SwiftUI
 import MapKit
+import Foundation
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id = UUID()
+    var latitude, longitude: Double
+    //Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
 
 struct Maps: View {
     // criando área de localizacao pelo MapKit passando coordenadas do local de desejo e posteriormente aproximacao
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.334795, longitude: -122.009007), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.00))
     
+    let locations: [NationalParkLocation] = [
+          NationalParkLocation.init(latitude: 37.334795, longitude: -122.009007),
+      ]
     
     // corpo da classe
     var body: some View {
         
         // referencia
-        Map(coordinateRegion: $region)
+        Map(coordinateRegion: $region, annotationItems: locations) { item in
+            MapAnnotation(coordinate: item.location) {
+                    Button {
+                        print("Location is", item.location)
+                    } label: {
+                        Text("🥳")
+                            .font(.largeTitle)
+                    }
+                }
+        }
             .frame(minWidth: 700, idealWidth: .infinity, minHeight: 700, alignment: .leading)
             .edgesIgnoringSafeArea(.all) // ignora o tanto quanto possível
             .padding(.bottom) // mas respeita a área bottom
