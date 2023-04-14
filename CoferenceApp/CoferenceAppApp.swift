e3835b2f4d4d324a73ff79664e73c94e1fb30b64
filipeilunga <filipeilunga@gmail.com>
2023-04-14 13:23:19 -0300

    removendo solicitacao para notificação

--- CoferenceApp/CoferenceAppApp.swift
+++ CoferenceApp/CoferenceAppApp.swift
@@ -9,15 +9,6 @@ import SwiftUI
 
 @main
 struct CoferenceAppApp: App {
-    init() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-            if success {
-                print("All set!")
-            } else if let error = error {
-                print(error.localizedDescription)
-            }
-        }
-    }
     var body: some Scene {
         WindowGroup {
             Jorge()
